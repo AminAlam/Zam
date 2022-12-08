854e52a97f1ea2ff8f372bf2956331cce71a9c10
Amin Alam <ma.alamalhoda@gmail.com>
2022-12-08 13:11:53 +0100

    configs laoding utils problem solved

--- src/configs.py
+++ src/configs.py
@@ -8,7 +8,6 @@ import time
 from pathlib import Path
 import sqlite3
 import datetime as dt
-import utils
 
 from telegram import Bot, InputMediaPhoto, InputMediaVideo, MessageEntity, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, MessageEntity
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
@@ -21,4 +20,7 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 creds_file = f"{working_dir}/creds.json"
 db_conf_file = f"{working_dir}/db_conf.json"
 
-time_diff = {'hours':1, 'minutes':30}
+time_diff = {'hours':2, 'minutes':30}
+
+
+import utils
