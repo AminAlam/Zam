b504ba08df1cc2862846a7da03d40cc2c1c8164c
Amin Alam <ma.alamalhoda@gmail.com>
2023-04-27 16:33:05 +0200

    reference_snapshot argument added to arg parser

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from telegram_backend import TelegramAdminBot, TelegramSuggestedTweetsBot
 @click.option('--time_diff', default='0:30', help="Difference between the time of the tweet and the time of the server running the bot. Format: HOURS:MINUTES")
 @click.option('--mahsa_message', default=True, help="A message about Mahsa Amini's murder will be sent to the channel with a timer which is updated evety few seconds.")
 @click.option('--gpt_suggestions_rate', default=0, help="A GPT language model will randomly complete some short tweets with this rate. 0 means no suggestoins")
-@click.option('--reference_snapshot', default=True, help="A snapshot of the reference tweets (Qouted tweet or the tweet which main tweet is a reply to) will be set as one of the media of the post. Note that this feature needs chromium to functoin")
+@click.option('--reference_snapshot', default=True, help="A snapshot of the reference tweets (Qouted tweet or the tweet which main tweet is a reply to) will be set as one of the media of the post. Note that this feature needs chromium to function")
 @click.option('--num_tweets_to_preserve', default=1000, type=click.IntRange(500, 5000, clamp=True), help="Number of tweets to be saved in the database. Only last num_tweets_to_preserve tweets in the line will be preserved and the old ones will be deleted")
 def main(time_diff, mahsa_message, gpt_suggestions_rate, reference_snapshot, num_tweets_to_preserve):
     creds = utils.read_credentials(creds_file)
@@ -18,8 +18,8 @@ def main(time_diff, mahsa_message, gpt_suggestions_rate, reference_snapshot, num
     except:
         raise Exception("the time_diff should be in format of HOURS:MINUTES")
     twitter_api = TwitterClient(creds, db_log)
-    suggestions_bot = TelegramSuggestedTweetsBot(creds, twitter_api, db_log, time_diff, gpt_suggestions_rate)
-    admin_bot = TelegramAdminBot(creds, twitter_api, db_log, suggestions_bot, time_diff, mahsa_message, gpt_suggestions_rate, num_tweets_to_preserve) 
+    suggestions_bot = TelegramSuggestedTweetsBot(creds, twitter_api, db_log, time_diff, gpt_suggestions_rate, reference_snapshot)
+    admin_bot = TelegramAdminBot(creds, twitter_api, db_log, suggestions_bot, time_diff, mahsa_message, gpt_suggestions_rate, num_tweets_to_preserve, reference_snapshot) 
 
 
 if __name__ == '__main__':
