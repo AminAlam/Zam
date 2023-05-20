0f4a0d3436b1237f6b64b81d320d1794d4f99a60
Amin Alam <ma.alamalhoda@gmail.com>
2023-05-20 15:56:08 +0200

    user_tweet_limit added

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ from telegram_backend import TelegramAdminBot, TelegramSuggestedTweetsBot
 @click.option('--mahsa_message', default=True, help="A message about Mahsa Amini's murder will be sent to the channel with a timer which is updated evety few seconds.")
 @click.option('--reference_snapshot', default=True, help="A snapshot of the reference tweets (Qouted tweet or the tweet which main tweet is a reply to) will be set as one of the media of the post. Note that this feature needs chromium to function")
 @click.option('--num_tweets_to_preserve', default=1000, type=click.IntRange(500, 5000, clamp=True), help="Number of tweets to be saved in the database. Only last num_tweets_to_preserve tweets in the line will be preserved and the old ones will be deleted")
-def main(time_diff, mahsa_message, reference_snapshot, num_tweets_to_preserve):
+@click.option('--user_tweet_limit', default=10, type=click.IntRange(1, 120, clamp=True), help="Maximum number of the tweets that an user can send during 1 hour period")
+def main(time_diff, mahsa_message, reference_snapshot, num_tweets_to_preserve, user_tweet_limit):
     creds = utils.read_credentials(creds_file)
     db_log = Database()
     try:
@@ -17,7 +18,7 @@ def main(time_diff, mahsa_message, reference_snapshot, num_tweets_to_preserve):
     except:
         raise Exception("the time_diff should be in format of HOURS:MINUTES")
     twitter_api = TwitterClient(creds, db_log)
-    suggestions_bot = TelegramSuggestedTweetsBot(creds, twitter_api, db_log, time_diff, reference_snapshot)
+    suggestions_bot = TelegramSuggestedTweetsBot(creds, twitter_api, db_log, time_diff, reference_snapshot, user_tweet_limit)
     admin_bot = TelegramAdminBot(creds, twitter_api, db_log, suggestions_bot, time_diff, mahsa_message, num_tweets_to_preserve, reference_snapshot) 
 
 
